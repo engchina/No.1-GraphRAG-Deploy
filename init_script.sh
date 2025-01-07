8403868abd92a77d6f6dc8400c2ff443b5817fcf
engchina <atjapan2015@gmail.com>
2025-01-07 13:29:11 +0800

    initial commit

--- init_script.sh
+++ init_script.sh
@@ -3,22 +3,6 @@ echo "Initializing application setup..."
 
 # Oracle Instant Client setup
 cd /u01/aipoc
-wget https://download.oracle.com/otn_software/linux/instantclient/2360000/instantclient-basic-linux.x64-23.6.0.24.10.zip -O instantclient.zip
-unzip instantclient.zip -d ./
-wget http://ftp.de.debian.org/debian/pool/main/liba/libaio/libaio1_0.3.113-4_amd64.deb
-dpkg -i libaio1_0.3.113-4_amd64.deb
-echo "/u01/aipoc/instantclient_23_6" > /etc/ld.so.conf.d/oracle-instantclient.conf
-ldconfig
-echo 'export LD_LIBRARY_PATH=/u01/aipoc/instantclient_23_6:$LD_LIBRARY_PATH' >> /etc/profile
-source /etc/profile
-unzip wallet.zip -d ./wallet
-cp ./wallet/* ./instantclient_23_6/network/admin/
-
-# Miniconda setup
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
-bash miniconda.sh -b -p /u01/aipoc/miniconda
-eval "$(/u01/aipoc/miniconda/bin/conda shell.bash hook)"
-/u01/aipoc/miniconda/bin/conda init bash
 
 # Git clone source code
 cd /u01/aipoc/No.1-GraphRAG
@@ -56,5 +40,6 @@ chmod +x main.sh
 ./main.sh &
 
 chmod +x ./genai_gateway/main.sh
+# Must run in the after setting
 # nohup ./genai_gateway/main.sh &
 echo "Initialization complete."
